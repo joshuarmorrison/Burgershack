3b1684bb0be1808aeb507b6319448cf29e7a19e0
Josh <joshuarmorrison@gmail.com>
2021-09-13 15:29:58 -0600

    finished

--- server/db/fakeDb.js
+++ server/db/fakeDb.js
@@ -1,9 +1,9 @@
 export const fakeDb = {
   burgers: [
-    { id: '29', name: 'Burnin' },
-    { id: '26', name: 'Cheesin' },
-    { id: '27', name: 'Quein' },
-    { id: '28', name: 'Hangover' }]
+    { id: '26', name: 'Burnin' },
+    { id: '27', name: 'Cheesin' },
+    { id: '28', name: 'Quein' },
+    { id: '29', name: 'Hangover' }]
 }
 
 // fakeDb.burgers.map(b=> new Burger(b))
